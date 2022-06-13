7e0abc7f1de92f3735b3e1d4635fdabfd8d4062f
crlsalejandro <aleocampoiza@gmail.com>
2022-06-12 22:08:08 -0300

    Firts commit

---
+++ my-app/src/potencia.js
@@ -0,0 +1,11 @@
+function potencia(base, exponente) {
+    let resultado = 1;
+  
+    for (let i = 0; i < exponente; i++) {
+      resultado *= base;
+    }
+    return resultado;
+  }
+  module.exports = potencia;
+  
+  
---
+++ my-app/src/potencia.test.js
@@ -0,0 +1,5 @@
+const potencia = require('./potencia');
+
+test('Potencia', () => {
+       expect(potencia(2,2)).toBe(4);
+     });
